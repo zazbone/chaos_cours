078d61a40fbfdfa7b2e8717a110279806985216f
Yann Querlioz <coczaz@gmail.com>
2020-11-17 12:43:17 +0100

    edit main

--- chaos/__main__.py
+++ chaos/__main__.py
@@ -1,5 +1,14 @@
 from chaos.attractor.Henon import henon
 from chaos.attractor.Lozi import lozi
+from chaos.attractor.Lorentz import lorentz
 
-henon()
-lozi()
+from sys import argv
+
+args = argv[1:]
+
+if args[0].lower() == "lorentz":
+    lorentz()
+elif args[0].lower() == "henon":
+    henon()
+elif args[0].lower() == "lozi":
+    lozi()
--- chaos/attractor/Lorentz.py
+++ chaos/attractor/Lorentz.py
@@ -15,25 +15,26 @@ def dz(t, point: np.ndarray, sigma, rho, beta):
     return point[0] * point[1] - beta * point[2] 
 
 
-sample = 5000
+def lorentz():
+    sample = 5000
 
-rho = 28
-sigma = 10
-beta = 8 / 3
+    rho = 28
+    sigma = 10
+    beta = 8 / 3
 
-point = np.array([0, 0.1, 0])
+    point = np.array([0, 0.1, 0])
 
-TRAJECTORY = np.zeros(shape=(3, sample))
+    TRAJECTORY = np.zeros(shape=(3, sample))
 
-t0 = 0
-dt = 0.01
+    t0 = 0
+    dt = 0.01
 
-for i in range(sample):
-    point = point + rk.integr(t0=t0, dt=dt, CI=point, func=(dx, dy, dz), rho=rho, sigma=sigma, beta=beta)
-    TRAJECTORY[... ,i] = point
+    for i in range(sample):
+        point = point + rk.integr(t0=t0, dt=dt, CI=point, func=(dx, dy, dz), rho=rho, sigma=sigma, beta=beta)
+        TRAJECTORY[... ,i] = point
 
 
-fig = plt.figure()
-ax = plt.axes(projection="3d")
-ax.plot3D(xs=TRAJECTORY[0], ys=TRAJECTORY[1], zs=TRAJECTORY[2], color="blue")
-plt.show()
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+    ax.plot3D(xs=TRAJECTORY[0], ys=TRAJECTORY[1], zs=TRAJECTORY[2], color="blue")
+    plt.show()
