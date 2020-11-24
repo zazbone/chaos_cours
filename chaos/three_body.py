fedc8ee053ac64719a589195b0add7b06e5e2a3b
zazbone <coczaz@gmail.com>
2020-11-24 22:48:31 +0100

    solar system

--- chaos/three_body.py
+++ chaos/three_body.py
@@ -2,67 +2,128 @@ import numpy as np
 
 from chaos import runge_kutta as rk
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import csv
 
 
-G = 0.1
+G = 1
 D = 3
+
+
+class Body:
+    def __init__(self, r, v, m):
+        if np.shape(r) != np.shape(v):
+            raise ValueError("Speed and position vector must have same dimension")
+        self.r = np.array(r)
+        self.v = np.array(v)
+        self.m = m
+
+
+class ThreeBody():
+    G = 6.7e-11
+
+    def __init__(self, r1, r2, r3, v1, v2, v3, m1, m2, m3):
+        self.r = np.array([r1, r2, r3])
+        self.v = np.array([v1, v2, v3])
+        self.m = np.array([m1, m2, m3])
+
+    @classmethod
+    def from_body(cls, body1: Body, body2: Body, body3: Body):
+        return cls(
+            r1=body1.r, r2=body2.r, r3=body3.r,
+            v1=body1.v, v2=body2.v, v3=body3.v,
+            m1=body1.m, m2=body2.m, m3=body3.m,
+        )
+
+    def p(self):
+        return np.array([
+            [self.m[0], 0, 0],
+            [0, self.m[1], 0],
+            [0, 0, self.m[0]],
+        ]) * self.v
+
+    @classmethod
+    def a(cls):
+        return cls.a1, cls.a2, cls.a3
+
+    @classmethod
+    def a1(cls, t, r, m1, m2, m3):
+        return cls.G * (m2 * ratio(r[0], r[1]) + m3 * ratio(r[0], r[2]))
+
+    @classmethod
+    def a2(cls, t, r, m1, m2, m3):
+        return cls.G * (m1 * ratio(r[1], r[0]) + m3 * ratio(r[1], r[2]))
+
+    @classmethod
+    def a3(cls, t, r, m1, m2, m3):
+        return cls.G * (m1 * ratio(r[2], r[0]) + m2 * ratio(r[2], r[1]))
+
+    @classmethod
+    def dr(cls):
+        return cls.dr1, cls.dr2, cls.dr3
+
+    @staticmethod
+    def dr1(t, v):
+        return v[0]
+
+    @staticmethod
+    def dr2(t, v):
+        return v[1]
+
+    @staticmethod
+    def dr3(t, v):
+        return v[2]
 
 
 def ratio(ra, rb):
     return (rb - ra) / abs(rb - ra) ** 3
 
 
-def dp_b1(t, r, m1, m2, m3):
-    return G * m1 * (m2 * ratio(r[0], r[1]) + m3 * ratio(r[0], r[2]))
-
-def dp_b2(t, r, m1, m2, m3):
-    return G * m2 * (m1 * ratio(r[1], r[0]) + m3 * ratio(r[1], r[2]))
-
-def dp_b3(t, r, m1, m2, m3):
-    return G * m3 * (m1 * ratio(r[2], r[0]) + m2 * ratio(r[2], r[1]))
-
-def dr_b1(t, p, m1, m2, m3):
-    return p[0] / m1
-
-def dr_b2(t, p, m1, m2, m3):
-    return p[1] / m2
-
-def dr_b3(t, p, m1, m2, m3):
-    return p[2] / m3
+def write_data(writer, fieldnames, gen, time, r, v):
+    row = {"gen": gen, "time": time}
+    for i in range(1, 4):
+        row[f"x{i}"] = r[i - 1][0]
+        row[f"y{i}"] = r[i - 1][1]
+        row[f"z{i}"] = r[i - 1][2]
+        row[f"v_x{i}"] = v[i - 1][0]
+        row[f"v_y{i}"] = v[i - 1][1]
+        row[f"v_z{i}"] = v[i - 1][2]
+    writer.writerow(row)
 
 
-def main():
-    m1, m2, m3 = 1, 1, 1
-    r = np.random.uniform(-10, 10, size=(3, D))
-    p = np.random.uniform(-10, 10, size=(3, D))
-    print(r)
+def solar_sys():
+    # Initial orbital parameters
+    MOON = Body(
+        [1.5038e11, 0, 0],
+        [0, 4e4, 0],
+        7e22
+    )
+    EARTH = Body(
+        [1.5e11, 0, 0],
+        [0, 3e4, 0],
+        6e24
+    )
+    SUN = Body(
+        [0, 0, 0],
+        [0, 0, 0],
+        2e30
+    )
 
-    sample = 1024
+    system = ThreeBody.from_body(MOON, EARTH, SUN)
+
     t0 = 0
-    dt = 0.01
     t = t0
+    sample = 1024
+    dt = (4 * 30.5 * 24 * 3600) / 1024  # 4 mois d'étude
 
-    R1 = np.zeros(shape=(sample, D))
-    R2 = np.zeros(shape=(sample, D))
-    R3 = np.zeros(shape=(sample, D))
+    with open("result.csv", "w") as csv_file:
+        fields_names = ["gen", "time"]
+        for i in range(1, 4):
+            fields_names.extend((f"Body{i}", f"x{i}", f"y{i}", f"z{i}", f"v_x{i}", f"v_y{i}", f"v_z{i}"))
+        writer = csv.DictWriter(csv_file, fieldnames=fields_names)
+        writer.writeheader()
 
-    for i in range(sample):
-        p += rk.integr(t0=t, dt=dt, CI=r, func=(dp_b1, dp_b2, dp_b3), m1=m1, m2=m2, m3=m3)
-        r += rk.integr(t0=t, dt=dt, CI=p, func=(dr_b1, dr_b2, dr_b3), m1=m1, m2=m2, m3=m3)
-        R1[i] = r[0]
-        R2[i] = r[1]
-        R3[i] = r[2]
-        t += dt
-
-
-    ax = plt.axes(projection="3d")
-    ax.plot3D(xs=R1[::][0], ys=R1[::][1], zs=R1[::][2], color="blue")
-    ax.plot3D(xs=R2[::][0], ys=R2[::][1], zs=R2[::][2], color="red")
-    ax.plot3D(xs=R3[::][0], ys=R3[::][1], zs=R3[::][2], color="green")
-    plt.show()
-
-
-if __name__ == "__main__":
-    main()
+        for i in range(sample):
+            write_data(writer, fields_names, i, t, system.r, system.v)
+            system.v += rk.integr(t0=t, dt=dt, CI=system.r, func=ThreeBody.a(), m1=system.m[0], m2=system.m[1], m3=system.m[2])
+            system.r += rk.integr(t0=t, dt=dt, CI=system.v, func=ThreeBody.dr())
+            t += dt
